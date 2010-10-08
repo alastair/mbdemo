bc37b5731ae48449f49d63ded433af611c7594d2
Alastair Porter <alastair@porter.net.nz>
2010-10-07 22:44:25 -0400

    Audio fingerprinting

--- fp-demo.py
+++ fp-demo.py
@@ -4,16 +4,14 @@ import sys
 import musicbrainz2 as mb
 import musicbrainz2.webservice as ws
 import musicbrainz2.model as model
-import sys
-import os
-sys.path.insert(0, os.path.expanduser("~/Projects/pyofa"))
-import fingerprint
-import musicdns
+from pyofa import fingerprint
+from pyofa import musicdns
 
-# http://www.musicbrainz.org/ws/1/track/?puid=e0488c71-2d76-f3b8-6b63-59c21fe93cb1&type=xml
-# http://www.musicbrainz.org/ws/1/track/29dd9077-f6dd-4b56-97c0-92ca31d273d8?inc=artist+releases&type=xml
-# http://www.musicbrainz.org/ws/1/release/93c971ac-b822-4be5-9e0c-b8f1210a291e?inc=artist+tracks&type=xml
-# http://musicbrainz.org/release/93c971ac-b822-4be5-9e0c-b8f1210a291e.html 
+# Steps:
+#  1. Fingerprint audio file
+
+# Webservice queries used:
+#  POST to http://ofa.musicdns.org:80/ofa/1/track
 
 MUSICDNS_KEY = 'a7f6063296c0f1c9b75c7f511861b89b'
 
@@ -22,8 +20,11 @@ def demo(file):
 	fp, dur = fingerprint.fingerprint_any(file)
 	print "Fingerprint %s" % fp
 	title, artist, puid = musicdns.lookup_fingerprint(fp, dur, MUSICDNS_KEY)
-	#puid = "e0488c71-2d76-f3b8-6b63-59c21fe93cb1"
+	#puid = "fe0b59ea-d498-a4f3-c52a-23050e3790d9"
 	print "Title %s, artist %s, puid %s" % (title, artist, puid)
 
 if __name__ == '__main__':
-	demo(sys.argv[1])
+	if len(sys.argv) == 2:
+		demo(sys.argv[1])
+	else:
+		print "Usage: %s <file>" % sys.argv[0]
