989dc189757ec4aec88737ae1c95219d3eeb167c
Alastair Porter <alastair@porter.net.nz>
2010-10-07 22:04:41 -0400

    Get detailed release information

--- demo1.py
+++ demo1.py
@@ -5,9 +5,16 @@ import musicbrainz2 as mb
 import musicbrainz2.webservice as ws
 import musicbrainz2.model as model
 
-# http://www.musicbrainz.org/ws/1/release/?title=Songs+in+the+Key+of+Life&artist=Stevie+Wonder&type=xml
+# Steps:
+#  1. Search for a release and print results
+#  2. Get detailed release information
+
+# Webservice queries used:
+#  http://www.musicbrainz.org/ws/1/release/?title=Songs+in+the+Key+of+Life&artist=Stevie+Wonder&type=xml
+#  http://www.musicbrainz.org/ws/1/release/edf99004-eaca-4295-b2f9-581891ba044f?inc=artist+tracks&type=xml
+#  http://www.musicbrainz.org/ws/1/release/24a355b3-940d-4428-b54d-d830fb5766f6?inc=artist+tracks&type=xml
+
 # http://musicbrainz.org/release/e3a0afcc-79fa-44da-ab29-0da9973eac24.html
-# http://www.musicbrainz.org/ws/1/release/edf99004-eaca-4295-b2f9-581891ba044f?inc=artist+tracks&type=xml
 
 def demo(artist, release):
 	print "Searching for '%s' by %s" % (release, artist)
@@ -16,20 +23,27 @@ def demo(artist, release):
                                 artistName=artist)
         rels = q.getReleases(filter=filter)
 
-	print "number results: %d" % len(rels)
+	print "number of results: %d" % len(rels)
 	if len(rels) > 0:
 		r = rels[0].getRelease()
 
-		# Get the release information
+		# By default the webservice only returns basic information, so we
+		#  need to ask it to give us artist and track information
 		includes = ws.ReleaseIncludes(artist=True, tracks=True)
 		release = q.getReleaseById(r.id, include=includes)
 		print "Release 1: %s (%s) " % (release.getTitle(), release.id)
-		# XXX: Print the artist details
+		print "By %s" % release.getArtist().getName();
 		print "Tracks:"
 		trackno = 1
 		for track in release.getTracks():
-			print "%d: %s " % (trackno, track.getTitle())
+			print "%d: %s" % (trackno, track.getTitle()),
+			if track.getArtist() is not None:
+				print "[by %s]" % track.getArtist().getName()
+			else: print
 			trackno += 1
 
 if __name__ == '__main__':
-	demo(sys.argv[1], sys.argv[2])
+	if len(sys.argv) == 3:
+		demo(sys.argv[1], sys.argv[2])
+	else:
+		print "Usage: %s <name> <title>" % sys.argv[0]
